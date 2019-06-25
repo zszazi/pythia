a9eeccf3785bf07b504765c9a8a25e199a61804f
zszazi <41579863+zszazi@users.noreply.github.com>
2019-06-25 20:21:25 +0530

    Update vocab.py

--- pythia/utils/vocab.py
+++ pythia/utils/vocab.py
@@ -122,7 +122,7 @@ class BaseVocab:
 
             with open(vocab_file, "r") as f:
                 lines = f.readlines()
-                lines = [line.decode("utf-8").strip("\n") for line in lines]
+                lines = [line.strip("\n") for line in lines]
                 for line in lines:          
                     self.itos[index] = line.strip()
                     self.word_dict[line.strip()] = index
